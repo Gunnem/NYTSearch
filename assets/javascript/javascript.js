da444c10fb4247cd8a8e1a696857b4206d424dac
Gunnem <sp3dng9@gmail.com>
2018-10-20 13:38:03 -0700

    search terms added

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,7 +1,9 @@
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 queryURL += '?' + $.param({
  'api-key': "749c632c403b429383ae2c86692e4500",
- 'q': "car"
+ 'q': "car",
+ 'begin_date': "19900101",
+ 'end_date': "20000101"
 });
 $.ajax({
 url: queryURL,
