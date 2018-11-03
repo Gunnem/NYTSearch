3e200c14594b653dc23ba36497adddc1aa5aa5a1
ashleywatts3 <ashleywatts3@users.noreply.github.com>
2018-11-03 12:57:16 -0700

    changing everything

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,7 @@
 
  
- 
+//Initializing the Firebase//
+
  var config = {
    apiKey: "AIzaSyB59Y9d2vxVuuUhuYgL0jqdjBJs6RvAEgI",
    authDomain: "eventfinder-f4e7e.firebaseapp.com",
@@ -11,3 +12,15 @@
  };
  firebase.initializeApp(config);
 
+ // Variable to reference the Database//
+ var database = firebase.database();
+
+
+
+$(document).ready(function () {
+    //Convert address tags to google map links - Copyright Michael Jasper 2011
+    $('address').each(function () {
+        var link = "<a href='http://maps.google.com/maps?q=" + encodeURIComponent( $(this).text() ) + "' target='_blank'>" + $(this).text() + "</a>";
+        $(this).html(link);
+    });
+});
