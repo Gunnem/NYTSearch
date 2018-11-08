375fd5f40d46d0204da7e1d6ca2422bb160cda94
Laza <postlaza@gmail.com>
2018-11-07 19:21:28 -0800

    API

--- assets/javascript/zomato.js
+++ assets/javascript/zomato.js
@@ -1,10 +1,15 @@
 // Set global variables
 // These variables are filled in for now. They'll be what user enters.
-var cityName = "Manhattan";
-var cuisineType = "afghani";
+var cityName = "Berkeley";
+// var cuisineType = "afghani";
 var cityNumber= [];
-var cityCuisine= [];
 console.log(cityNumber);
+
+var cityCuisine= [];
+var latPush = []
+var lngPush = []
+
+
 console.log(cityCuisine);
 
 // Function to use prompt result from city name search box, and gets enitiy_ID from Zomato's API. Pushes result to cityNumber
@@ -12,6 +17,7 @@ console.log(cityCuisine);
 
     var queryURL1 = 
     "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/locations?query="+ cityName
+// console.log(cityName);
 
       $.ajax({
           url:queryURL1,
@@ -22,44 +28,49 @@ console.log(cityCuisine);
           }
         
       }).then(function(response){
-        console.log('my object response', response)
+        // console.log('my object response', response)
 
         var city = response.location_suggestions[0].entity_id
         console.log(city)
-        cityNumber.push(city);
+        cityNumber.push(parseFloat(city));
         })
   }
     entityID()
 
 // Function that takes cityNumber and enters it in Zomato's search parameter. This retrieves all the cuisines and their corresponding cuisine ID numbers available in that city. Pushes results to cityCuisine array.
-    function cuisineID(){
+//     function cuisineID(){
+//       setTimeout(function(){
+//       var queryURL2 = 
+//       "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/cuisines?city_id="+ cityNumber[0]
+// console.log(cityNumber);
 
-      var queryURL2 = 
-      "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/cuisines?city_id="+ cityNumber[0]
-
-        $.ajax({
-            url:queryURL2,
-            type:"GET",
-            crossDomain: true,
-            headers:{
-              "user_key": "730e5838ac13eeafae0600c8b1d26457",
-            }
+//         $.ajax({
+//             url:queryURL2,
+//             type:"GET",
+//             crossDomain: true,
+//             headers:{
+//               "user_key": "730e5838ac13eeafae0600c8b1d26457",
+//             }
           
-        }).then(function(response){
-          console.log('my cuisine response', response)
+//         }).then(function(response){
+//           console.log('my cuisine response', response)
   
-          // var cuisine = response.location_suggestions[0].entity_id
-          // console.log(cuisine)
-          cityCuisine.push(respnse);
-          })
-    }
-    cuisineID()
+//           var cuisine = response.cuisines[0].cuisine
+//           console.log(cuisine)
+//           cityCuisine.push(cuisine);
+//           })
+//         }, 1000);
+//     }
+//     cuisineID()
 
     // Function that uses cityNumber and cityCuisine results into the query paramters to retrieve restaurant info.
     function restInfo(){
-
+      setTimeout(function(){
         var queryURL3 = 
-        "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/search?entity_id=" + cityNumber[0] + "&entity_type=zone&radius=25&cuisines=" + cityCuisine[0]
+        "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/search?city_id=" + cityNumber[0] + "&entity_type=zone&radius=25&cuisines=1"
+    // console.log(cityNumber[0]);
+    // console.log(cityCuisine[0]);
+    
     
           $.ajax({
               url:queryURL3,
@@ -70,11 +81,23 @@ console.log(cityCuisine);
               }
             
           }).then(function(response){
-            console.log('my object response', response)
-            })
-      }
-        restInfo()  
-
-
-
-
+            // console.log('my object response', response)
+            for ( i = 0; i < response.restaurants.length; i++) {
+              
+              var lat = response.restaurants[i].restaurant.location.latitude
+             
+              var lng = response.restaurants[i].restaurant.location.longitude
+              
+              latPush.push(parseFloat(lat))
+              lngPush.push(parseFloat(lng))
+              
+             
+             }
+            
+            }) 
+          }, 2000); 
+          }
+         
+          restInfo()  
+         
+          
