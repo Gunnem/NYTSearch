58b58ad9d8aff30d30de036caf64a081a2df175b
Gunnem <sp3dng9@gmail.com>
2018-11-09 19:50:33 -0800

    list function

--- assets/javascript/zomato.js
+++ assets/javascript/zomato.js
@@ -8,31 +8,57 @@ console.log(cityCuisine);
 var latPush = []
 var lngPush = []
 
+var restNameArray = [];
+console.log(restNameArray);
+var restAddressArray = [];
+console.log(restAddressArray);
+var restUrlArray = [];
+console.log(restUrlArray);
+
+var testArray = ["one","two","three"]
+var testArray2 = ["a","b","c"]
+
+// var combinedArray ={restNameArray, restAddressArray, restUrlArray};
+// console.log(combinedArray)
+
+for (var i = 0; i < restNameArray.length; i++) {
+
+  $("#ordList").append($("<li><p>" + restNameArray[i] + "<br>" + restAddressArray2[i] + "</p></li>"))
+}
+
+console.log($('#ordList'));
+// var compiledList = '<ol>'
+
+// combinedArray.forEach(function(combined){
+//  compiledList += '<li>'+ combined + '</li>'; 
+// });
+
+// compiledList = '<ol>'
 
 // Function to use prompt result from city name search box, and gets enitiy_ID from Zomato's API. Pushes result to cityNumber
-  function entityID(){
+  // function entityID(){
 
-    var queryURL1 = 
-    "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/locations?query="+ cityName
+  //   var queryURL1 = 
+  //   "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/locations?query="+ cityName
 // console.log(cityName);
 
-      $.ajax({
-          url:queryURL1,
-          type:"GET",
-          crossDomain: true,
-          headers:{
-            "user_key": "730e5838ac13eeafae0600c8b1d26457",
-          }
+  //     $.ajax({
+  //         url:queryURL1,
+  //         type:"GET",
+  //         crossDomain: true,
+  //         headers:{
+  //           "user_key": "730e5838ac13eeafae0600c8b1d26457",
+  //         }
         
-      }).then(function(response){
-        // console.log('my object response', response)
+  //     }).then(function(response){
+  //       // console.log('my object response', response)
 
-        var city = response.location_suggestions[0].entity_id
-        console.log(city)
-        cityNumber.push(parseFloat(city));
-        })
-  }
-    entityID()
+  //       var city = response.location_suggestions[0].entity_id
+  //       console.log(city)
+  //       cityNumber.push(parseFloat(city));
+  //       })
+  // }
+  //   entityID()
 
 // Function that takes cityNumber and enters it in Zomato's search parameter. This retrieves all the cuisines and their corresponding cuisine ID numbers available in that city. Pushes results to cityCuisine array.
 //     function cuisineID(){
@@ -62,9 +88,9 @@ var lngPush = []
 
     // Function that uses cityNumber and cityCuisine results into the query paramters to retrieve restaurant info.
     function restInfo(){
-      setTimeout(function(){
+     
         var queryURL2 = 
-        "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/search?city_id=" + cityNumber[0] + "&entity_type=zone&radius=25&cuisines=" + cityCuisine[0]
+        "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/search?city_id=280&entity_type=zone&radius=25&cuisines=168"
     // console.log(cityNumber[0]);
     // console.log(cityCuisine[0]);
     
@@ -88,13 +114,22 @@ var lngPush = []
               latPush.push(parseFloat(lat))
               lngPush.push(parseFloat(lng))
               
-             
-             }
+              
+              var rName = response.restaurants[i].restaurant.name
+              var rAddress = response.restaurants[i].restaurant.location.address
+              var rUrl = response.restaurants[i].restaurant.url
+
+              restNameArray.push(rName)
+              restAddressArray.push(rAddress)
+              restUrlArray.push(rUrl)
+}
             
             }) 
-          }, 2000); 
+         
           }
          
           restInfo()  
+
+
          
           
